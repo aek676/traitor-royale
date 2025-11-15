2310254b63b7ef8b521a148e319ac667795806af
aek676 <aek676@inlumine.ual.es>
2025-11-15 13:32:22 +0100

    feat: implementar lógica para iniciar el juego y gestionar jugadores

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,40 +1,5 @@
-import { defineAction } from 'astro:actions';
-import { z } from 'astro:schema';
-import type { Player } from '@/interfaces';
-import { getRandomCard } from '@/lib/clashRoyaleApi';
-import { randomInt } from 'crypto';
+import { game } from './game';
 
 export const server = {
-  startGame: defineAction({
-    input: z.object({
-      players: z.array(z.string()).min(3)
-    }),
-    handler: async (input, context) => {
-      const playerNames = input.players;
-
-      const shuffledPlayers = [...playerNames].sort(() => Math.random() - 0.5);
-
-      const impostorIndex = randomInt(0, shuffledPlayers.length);
-
-      const players: Player[] = shuffledPlayers.map((name, index) => ({
-        name,
-        nextPlayer: shuffledPlayers.length <= index + 1 ? '-1' : shuffledPlayers[index + 1],
-        isImpostor: index === impostorIndex
-      }));
-
-      const randomCard = await getRandomCard();
-      const randomPlayerToStart = playerNames[randomInt(0, playerNames.length)];
-
-      await context.session?.set('players', players);
-      await context.session?.set('randomCard', randomCard);
-      await context.session?.set('randomPlayerToStart', randomPlayerToStart);
-
-      return {
-        success: true,
-        playersCount: shuffledPlayers.length,
-        gameId: crypto.randomUUID(),
-        firstPlayer: players[0].name
-      };
-    },
-  }),
+  game,
 };

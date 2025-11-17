1dd779a11f4358bba0eec25831c491a437d87bec
aek676 <aek676@inlumine.ual.es>
2025-11-17 17:34:21 +0100

    feat: mejorar estilos y estructura de botones en MyButton, Welcome y ActionButton

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -6,14 +6,11 @@ interface MyButtonProps {
 
 export function MyButton({ text, onClick, type = "button" }: MyButtonProps) {
     return (
-        // 1. Cambiamos el <div> exterior por un <button>
-        // 2. Movemos las props 'onClick' y 'type' aquí
         <button className="couche1" onClick={onClick} type={type}>
             <div className="couche2">
                 <div className="couche23">
                     <div className="couche3">
                         <div className="couche4">
-                            {/* 3. Cambiamos el <button> interno por un <div> */}
                             <div className="battle">{text}</div>
                             <div className="couche5"></div>
                         </div>

59af0d877d34a89d55a9d0ebddfc78d3e01ea782
aek676 <aek676@inlumine.ual.es>
2025-11-23 22:54:33 +0100

    fix: adjust background size in Layout.astro for improved aesthetics

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as BattleBannerImpostor } from "./BattleBannerImpostor.png";
 export { default as BackgroundImage } from "./bg.png";
+export { default as KingBlue } from "./KingBlue.png";
+export { default as KingRed } from "./KingRed.png";
